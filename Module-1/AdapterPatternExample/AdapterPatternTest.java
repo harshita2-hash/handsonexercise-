239393f7621882a222857d1a0d006f3fcabca336
harshita2-hash <gallaharshita24@gmail.com>
2026-06-24 15:44:57 +0530

    small change

--- Module-1/AdapterPatternExample/AdapterPatternTest.java
+++ Module-1/AdapterPatternExample/AdapterPatternTest.java
@@ -2,13 +2,13 @@ public class AdapterPatternTest {
 
     public static void main(String[] args) {
 
-        // PayPal Payment
+        
         PaymentProcessor paypal =
                 new PayPalAdapter(new PayPalGateway());
 
         paypal.processPayment(5000);
 
-        // Stripe Payment
+
         PaymentProcessor stripe =
                 new StripeAdapter(new StripeGateway());
 
